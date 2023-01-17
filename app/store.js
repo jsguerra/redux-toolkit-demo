3518540ef81c1d2615e6d78193f8d773fe33a8aa
jsguerra <jose@gcostudios.com>
2023-01-17 17:14:23 +0000

    file  updated

--- app/store.js
+++ app/store.js
@@ -1,5 +1,6 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
+const icecreamReducer = require("../features/icecream/icecreamSlice");
 
 // Invoke the function and assign it to a constant
 // takes an object as an argument and specify key called reducer
@@ -8,6 +9,7 @@ const cakeReducer = require("../features/cake/cakeSlice");
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
+    icecream: icecreamReducer,
   },
 });
 
